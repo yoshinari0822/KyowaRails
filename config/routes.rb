b651eb2bafacdb8db6f45cce00318b229124ba6b
yoshinari0822 <yoshinari.kanayama0822@gmail.com>
2025-01-18 21:05:21 +0900

    まずはログインしてもらわないとダメなようにする

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :diaries
   resources :users
   get "welcome/index"
-  devise_for :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
