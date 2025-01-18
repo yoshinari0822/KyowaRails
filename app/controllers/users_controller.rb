abbf8c95d2e96b608b88d2129c6652ce884ca4f3
yoshinari0822 <yoshinari.kanayama0822@gmail.com>
2025-01-18 20:07:26 +0900

    ユーザー情報を編集できるようにした

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [ :edit, :update ]
+
+  def edit
+  end
+
+  # 生成AIが作成。ユーザー情報を変更する
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: "ユーザー情報が更新されました。"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :icon)
+  end
 end
